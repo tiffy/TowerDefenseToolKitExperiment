9faeeabb1ef352a3afaba9eea2940560cac7fac6
tiffy <tiffytong@gmail.com>
2014-08-10 00:24:21 -0400

    Meow

---
+++ Assets/Tower.js
@@ -0,0 +1,29 @@
+﻿#pragma strict
+
+var muzzleEffect : GameObject;
+var errorAmount : float = 0.001;
+var myTarget : Transform;
+var muzzlePositions : Transform[];
+var turretBall : Transform;
+
+private var nextFireTime : float;
+private var nextMoveTime : float;
+private var desiredRotation : Quaternion;
+private var aimError : float;
+
+function Start () {
+
+}
+
+function Update () {
+
+if (myTarget) {
+if (Time.time >= nextMoveTime) {
+CalculateAimPosition(myTarget.position);
+turretBall.rotation = Quaternion.Lerp(turretBall.rotation, desiredRotation, Time.deltaTime*turn);
+}
+if (Time.time >= nextFireTime) {
+FireProjectile();
+}
+
+}
---
+++ Assets/bullet.js
@@ -0,0 +1,28 @@
+﻿#pragma strict
+
+
+var mySpeed : float = 10;
+var myRange : float = 10;
+
+
+
+private var myDist : float;
+
+function Start () {
+
+
+}
+
+
+
+function Update () {
+
+
+
+transform.Translate(Vector3.forward*Time.deltaTime*mySpeed);
+myDist += Time.deltaTime*mySpeed;
+
+if(myDist >= myRange)
+Destroy(gameObject);
+
+}
